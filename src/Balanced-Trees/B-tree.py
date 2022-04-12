d0d295b195a2b1ef2b10f4f56138e18cc076387a
yohager <guoyuhang0921@gmail.com>
2022-04-12 21:50:10 +0800

    balanced tree codes

---
+++ src/Balanced-Trees/B-tree.py
@@ -0,0 +1,24 @@
+import time
+import random 
+import bisect 
+
+class TreeNode:
+    def __init__(self) -> None:
+        self.vals = []
+        self.children = []
+        self.parent = None 
+
+
+class BTree:
+    def __init__(self,w) -> None:
+        self.root = TreeNode()
+        self.w = w 
+    
+    def Insert(self,v):
+        pass
+
+    def Search(self,v):
+        pass 
+
+    def Delete(self,v):
+        pass 
